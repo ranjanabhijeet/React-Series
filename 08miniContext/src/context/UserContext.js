5b4ca2dd79ce1e7df779f8b0ec2fb61f040e35f9
Abhijeet Ranjan <ranjanabhijeet2004@gmail.com>
2024-04-20 14:44:50 +0530

    Context api project

---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserContext = React.createContext();
+
+export default UserContext;
+
+//context provide a variable ...this is known as provider nd this is 1st step
