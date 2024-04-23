9fc87863a2b8f3dded96a8010de5e971dc8f1cfa
Abhijeet Ranjan <ranjanabhijeet2004@gmail.com>
2024-04-23 16:21:18 +0530

    todo project using local storage

---
+++ 10todocontextLocal/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import {TodoProvider} from './contexts'
+import './App.css'
+import { useEffect } from 'react'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components'
+
+function App() {
+  const [todos, setTodos] =useState([])
+// This todos is array which consists of many value 
+
+  const addTodo =(todo) => {
+    setTodos((prev) => [{id:Date.now(),...todo}, ...prev])
+
+    // if we pass todo inside setTodo like this setTodo(todo) then it delete all previous value nd add only one value
+    // Nd to get all prev value we have to pass the value like this setTodos((prev) => [...prev] )
+    // for adding new todo pass before prev value thewn pass the value like this setTodos((prev) => [todo, ...prev]);
+    // or want to add after then pass it like this setTodos((prev) => [...prev, todo]);
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
+
+  const deleteTodo = (id) =>{
+    setTodos((prev) => prev.filter((todo) => todo.id !== id )) 
+  }
+
+  const toggleComplete =(id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed}: prevTodo))
+  }
+
+  useEffect(() => {
+    JSON.parse(localStorage.getItem("todos"))
+    if (todos && todos.length > 0){
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  },[todos])
+
+  return (
+    <TodoProvider
+    value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">{/* Todo form goes here */}
+          <TodoForm/>
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id}
+              className='w-full'
+              >
+                <TodoItem todo ={todo}/>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
+  );
+}
+
+export default App
